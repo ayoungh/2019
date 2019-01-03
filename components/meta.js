0481638adfe2f12434066ce089806c1f9f4d1d99
Anthony Young <ayoung93@gmail.com>
2019-01-03 01:49:37 +0000

    Add google tag manager

--- components/meta.js
+++ components/meta.js
@@ -13,6 +13,14 @@ export default () => (
             <meta charSet="utf-8" />
             <link rel="shortcut icon" href="/static/favicon.ico" />
             <script defer src="https://use.fontawesome.com/releases/v5.6.3/js/all.js" integrity="sha384-EIHISlAOj4zgYieurP0SdoiBYfGJKkgWedPHH4jCzpCXLmzVsw1ouK59MuUtP4a1" crossOrigin="anonymous"></script>
+
+      <script dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KZQWZF');`}} />
+
         </Head>
         <style jsx global>{`
       body {
